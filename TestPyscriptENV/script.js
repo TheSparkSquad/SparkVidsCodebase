c6e6f1699fe8917d17e9e1aed44e28c3af947d8a
Maximillian Mahlke <themaxta@gmail.com>
2023-09-17 12:23:32 -0600

    moved scripts to ext file for security reasons

--- TestPyscriptENV/script.js
+++ TestPyscriptENV/script.js
@@ -2,3 +2,8 @@ function displayURL() {
     var url = document.getElementById('videoURL').value;
     document.getElementById('displayedURL').innerText = 'You entered: ' + url;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var summarizeButton = document.getElementById("summarizeButton");
+    summarizeButton.addEventListener("click", displayURL);
+});
