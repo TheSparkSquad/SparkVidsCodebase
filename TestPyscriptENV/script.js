584dde07583ceb5d1829979214c1b1ed26c45f20
Maximillian Mahlke <themaxta@gmail.com>
2023-09-17 12:40:55 -0600

    added documentation to script

--- TestPyscriptENV/script.js
+++ TestPyscriptENV/script.js
@@ -1,3 +1,19 @@
+/**
+ * SparkVidz A.I. Video Summarizer Script
+ * 
+ * This script provides functionalities for the video summarizer.
+ * It fetches and displays the content of a provided video URL in .srt format.
+ * During the prototype phase, it fetches the SRT content from a local file,
+ * but can be easily switched to use an actual API in the future.
+ * 
+ */
+
+
+
+
+/**
+ * Displays the entered URL and fetches the .srt content associated with it.
+ */
 function displayURL() {
     var url = document.getElementById('videoURL').value;
     document.getElementById('displayedURL').innerText = 'You entered: ' + url;
@@ -7,7 +23,11 @@ function displayURL() {
     });
 }
 
-
+/**
+ * Fetches the .srt content of a video from an external API.
+ * 
+ * @param {string} videoURL - The URL of the video to be summarized.
+ */
 function fetchSRT(videoURL) {
     var apiUrl = 'https://videotosrt.api/extract';
 
@@ -29,12 +49,19 @@ function fetchSRT(videoURL) {
     });
 }
 
-
+/**
+ * Initializes event listeners when the DOM content is fully loaded.
+ */
 document.addEventListener("DOMContentLoaded", function() {
     var summarizeButton = document.getElementById("summarizeButton");
     summarizeButton.addEventListener("click", displayURL);
 });
 
+/**
+ * Fetches the .srt content of a video from a local file (for prototype purposes).
+ * 
+ * @returns {Promise<string>} - A promise that resolves with the .srt content.
+ */
 function fetchLocalSRT() {
     return fetch('example.srt')
         .then(response => response.text())
