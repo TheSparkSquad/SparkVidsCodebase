de0a0a128406013b5259da20b7ec88d6c3deb049
Maximillian Mahlke <themaxta@gmail.com>
2023-09-30 10:23:55 -0600

    fixed runtime issues

---
+++ APIadapter.py
@@ -0,0 +1,12 @@
+# adapter.py
+
+##import requests  # Importing requests library for API interaction
+
+class Adapter:
+    def __init__(self, api_url):
+        self.api_url = api_url
+    
+    def get_data(self):
+        #response = requests.get(self.api_url)
+        #return response.json()  # Assuming API returns JSON data
+        print('test')
