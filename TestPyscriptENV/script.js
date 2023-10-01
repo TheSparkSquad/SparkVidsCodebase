139610ef9d3786e86e14a2089ddc7b2cfcf22bc6
Maximillian Mahlke <themaxta@gmail.com>
2023-10-01 09:27:53 -0600

    cleaned up crap

--- TestPyscriptENV/script.js
+++ TestPyscriptENV/script.js
@@ -8,53 +8,6 @@
  * 
  */
 
-
-
-
-
-// Global variable to track the Pyodide loaded state
-let pyodideLoaded = false;
-
-/**
- * Helper function to dynamically load a JS script
- * @param {string} src - Script source URL
- */
-function loadScript(src) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-    });
-}
-
-/**
- * Initialize Pyodide environment
- */
-async function loadPyodideAndPackages() {
-    if (pyodideLoaded) {
-        return;
-    }
-
-    // Load the Pyodide JavaScript library
-    await loadScript('https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js');
-
-    // Initialize Pyodide
-    let pyodide = await window.loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.21.3/full/' });
-
-    // You can also load specific Python packages if needed
-    // Example: await pyodide.loadPackage(['numpy', 'pandas']);
-
-    pyodideLoaded = true;
-    console.log("Pyodide initialized successfully!");
-}
-
-// Call the function directly to initialize Pyodide as soon as the script loads.
-loadPyodideAndPackages().catch(error => {
-    console.error("Error initializing Pyodide:", error);
-});
-
 /**
  * Displays the entered URL, fetches the .srt content associated with it, and the summary from a .txt file.
  */
@@ -95,31 +48,6 @@ function displayURL() {
     });
 }
 
-/**
- * Fetches the .srt content of a video from an external API.
- * 
- * @param {string} videoURL - The URL of the video to be summarized.
- */
-
-function fetchSRT(videoURL) {
-    var apiUrl = 'https://videotosrt.api/extract';
-
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ videoURL: videoURL })
-    })
-    .then(response => response.text())
-    .then(data => {
-        // Assuming the API returns the raw .srt file content.
-        // Display the SRT content or allow the user to download it.
-    })
-    .catch(error => {
-        console.error('Error fetching SRT:', error);
-    });
-}
 
 /**
  * Initializes event listeners when the DOM content is fully loaded.
@@ -128,6 +56,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var summarizeButton = document.getElementById("summarizeButton");
     summarizeButton.addEventListener("click", displayURL);
 });
+
+
 
 /**
  * Fetches the .srt content of a video from a local file (for prototype purposes).
